d9aa1692af6f03b12259b3086e3032ea7885a0d5
Jake Garcia <bballwiz4@gmail.com>
2021-08-08 15:24:05 -0700

    managing the contact state so that the message form can be shown and hidden.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import './index.css';
+import ShowInputBtn from './ShowInputBtn';
 
 class Contact extends Component {
 
+    state = {
+        showInputForm: false,
+    }
+
+    handleShowBtnClick = () => {
+        this.setState({
+            ...this.state,
+            showInputForm: !this.state.showInputForm,
+        })
+    }
+
     render() {
+
+        const { showInputForm } = this.state;
 
         return (
             <Container>
                 <h1>Contact</h1>
+                <Row>
+                    {showInputForm ? <ShowInputBtn handleShowBtnClick={this.handleShowBtnClick} show={showInputForm} /> : <ShowInputBtn handleShowBtnClick={this.handleShowBtnClick} show={showInputForm} />}
+                </Row>
+                {showInputForm &&
+                    <Row>Form</Row>
+                }
             </Container>
         )
     }
