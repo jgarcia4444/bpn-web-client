81782618efdf52ad2e069db1c4f17007ae84a0b3
Jake Garcia <bballwiz4@gmail.com>
2021-08-08 12:23:04 -0700

    The screen shots are now placeed and sized prooperly as well as given titles above them.

--- src/components/Home/ScreenShots/index.js
+++ src/components/Home/ScreenShots/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import homeScreen from '../../imgs/screenshots/homeScreen.png';
+import AddRecordScreen from '../../imgs/screenshots/AddRecordScreen.png';
+import recordsScreen from '../../imgs/screenshots/recordsScreen.png';
 
 import './index.css';
 
@@ -12,10 +15,17 @@ const ScreenShots = () => {
             </Row>
             <Row>
                 <Col xs={4}>
-                    img1
+                    <Row><h4>Home</h4></Row>
+                    <img className="screen-shot-img" src={homeScreen} />
                 </Col>
-                <Col xs={4}>img2</Col>
-                <Col xs={4}>img3</Col>
+                <Col xs={4}>
+                    <Row><h4>Records</h4></Row>
+                    <img className="screen-shot-img" src={recordsScreen} />
+                </Col>
+                <Col xs={4}>
+                    <Row><h4>Add Records</h4></Row>
+                    <img className="screen-shot-img" src={AddRecordScreen} />
+                </Col>
             </Row>
         </Container>
     )
