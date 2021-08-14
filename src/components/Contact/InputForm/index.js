249e0951c2608feed76ff78a874a6313550dee01
Jake Garcia <bballwiz4@gmail.com>
2021-08-13 23:21:51 -0700

    Added the input label to the message form.

--- src/components/Contact/InputForm/index.js
+++ src/components/Contact/InputForm/index.js
@@ -5,14 +5,28 @@ import './index.css';
 
 class InputForm extends Component {
 
+    state = {
+        messageUsername: ""
+    }
+
+    handleInputChange = ({target}) => {
+        this.setState({
+            ...this.state,
+            [target.name]: target.value
+        });
+    }
+
     render() {
+        const { handleInputChange } = this;
+        const { messageUsername } = this.state;
 
         return (
             <form>
                 <Container className="input-form-container">
                     <Row>
                         <Col sm={6}>
-
+                            <label htmlFor="messageUsername">Username</label>
+                            <input name="messageUsername" type="text" value={messageUsername} onChange={handleInputChange} />
                         </Col>
                     </Row>
                 </Container>
@@ -20,5 +34,11 @@ class InputForm extends Component {
         )
     }
 }
+
+// What will the forum have?
+// Username
+// Message
+// replies to the message
+// with username and message as well
 
 export default InputForm;
