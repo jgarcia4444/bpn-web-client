ca501e9b094f85f4c88a35a3186d5fa033aebdb2
Jake Garcia <bballwiz4@gmail.com>
2021-08-18 20:58:31 -0700

    The show reply form btn now has styling applied.

--- src/components/Contact/Forum/Message/ReplyFormBtn/index.js
+++ src/components/Contact/Forum/Message/ReplyFormBtn/index.js
@@ -5,8 +5,8 @@ import './index.css';
 const ReplyFormBtn = ({show, handleClick}) => {
 
     return (
-        <div onClick={handleClick} className="reply-form-btn-container">
-            { show ? "Hide" : "Show" } Message Form
+        <div onClick={handleClick} className="basic-btn reply-form-btn-container">
+            { show ? "Hide" : "Show" } Reply Form
         </div>
     )
 }
