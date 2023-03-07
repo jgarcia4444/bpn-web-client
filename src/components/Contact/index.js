8491b062194ebfea47a92051d20a8f8576e90e2b
Jake Garcia <bballwiz4@gmail.com>
2023-03-06 18:14:58 -0800

    All content for the support section added.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,6 +36,7 @@ const Contact = () => {
                 <h1 className="containerTitle">Support</h1>
                 <div className="supportDetailsContainer">
                     <div className="supportDetailsListContainer">
+                        <h3 className="supportDetailsListTitle">Email</h3>
                         <ul className="supportDetailsList">
                             <li className="supportDetailsItem">
                                 Checked at 8am and 5pm PST.
@@ -48,6 +49,21 @@ const Contact = () => {
                             </li>
                         </ul>
                         <span className="disclaimer">* All emails will not be replied at the exact times stated above they are estimates.</span>
+                    </div>
+                    <div className="supportDetailsListContainer">
+                        <h3 className="supportDetailsListTitle">Phone</h3>
+                        <ul className="supportDetailsList">
+                            <li className="supportDetailsItem">
+                                Call for questions or support.
+                            </li>
+                            <li className="supportDetailsItem">
+                                Text messages will be answered as early as possible
+                            </li>
+                            <li className="supportDetailsItem">
+                                Leave a voicemail if your phone call is not answered.
+                            </li>
+                        </ul>
+                        <span className="disclaimer">* Phone calls and texts will be responded as they come in.</span>
                     </div>
                 </div>
             </div>
