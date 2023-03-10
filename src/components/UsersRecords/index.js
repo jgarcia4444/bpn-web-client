686e4cc7357d30d2153bc26c50ce09e8400509e9
Jake Garcia <bballwiz4@gmail.com>
2023-03-09 23:38:25 -0800

    Added layout to the graph on user records and animated the loader in the same component.

--- src/components/UsersRecords/index.js
+++ src/components/UsersRecords/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
+import {FiLoader} from 'react-icons/fi';
+
+import '../../styles/UserRecords/index.css';
+
 import fetchRecords from '../../redux/actions/recordsActions/fetchRecords';
 
 const UsersRecords = ({recordsReducer, fetchRecords, userId}) => {
@@ -16,17 +20,23 @@ const UsersRecords = ({recordsReducer, fetchRecords, userId}) => {
     return (
         <div className="usersRecordsContainer">
             <h2 className="containerTitle">Records</h2>
-            {records.length !== 0  ?
-                <LineChart width={400} height={400} data={records}>
-                    <Line type="monotone" dataKey="systolic" stroke="#f00"/>
-                    <Line type="monotone" dataKey="diastolic" stroke="#f66"/>
-                    <XAxis name="dateRecorded" />
-                    <YAxis />
-                    <Tooltip />
-                </LineChart>
-            :
-                <p>No records have been recorded yet...</p>
-            }
+            <div className="graphContainer">
+                {loadingRecords === true ?
+                    <FiLoader className="loader" color="#f00" size={48} />
+                :
+                    records.length !== 0  ?
+                        <LineChart width={800} height={300} data={records}>
+                            <Line type="monotone" dataKey="systolic" stroke="#f00"/>
+                            <Line type="monotone" dataKey="diastolic" stroke="#f66"/>
+                            <XAxis />
+                            <YAxis />
+                            <Tooltip />
+                        </LineChart>
+                    :
+                        <p>No records have been recorded yet...</p>
+                    
+                }
+            </div>
 
         </div>
     )
--- src/config/network/Urls.js
+++ src/config/network/Urls.js
@@ -1,5 +1,7 @@
 
-// baseUrl: "http://192.168.4.162:3000"
+
+// baseUrl: "http://172.16.227.175:3000"
+
 export default {
-    baseUrl: "http://172.16.227.175:3000"
+    baseUrl: "http://192.168.4.162:3000"
 }
