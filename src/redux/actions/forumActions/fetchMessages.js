05100ac9c990780ab5998b39a1a1f336796aa9a1
Jake Garcia <bballwiz4@gmail.com>
2023-03-13 16:46:28 -0700

    Created the fetchMessages action.

---
+++ src/redux/actions/forumActions/fetchMessages.js
@@ -0,0 +1,24 @@
+import Urls from "../../../config/network/Urls";
+const {baseUrl} = Urls;
+
+const fetchMessages = () => {
+    let url = `${baseUrl}/messages`;
+    return async dispatch => {
+        dispatch({type: "LOADING_MESSAGES"});
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                let {success} = data;
+                if (success === data) {
+                    let {messages} = data;
+                    return dispatch({type: "MESSAGES_LOAD_SUCCESS", messages})
+                } else {
+                    let {error} = data;
+                    let {message} = error;
+                    return dispatch({type: "MESSAGES_LOAD_ERROR" ,message});
+                }
+            })
+    }
+}
+
+export default fetchMessages;
