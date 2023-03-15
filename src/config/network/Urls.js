342a8e4702361e450dba6e1be46ab151ae521fc7
Jake Garcia <bballwiz4@gmail.com>
2023-03-15 14:29:03 -0700

    Changed the networking url to the aws server.

--- src/config/network/Urls.js
+++ src/config/network/Urls.js
@@ -1,7 +1,8 @@
 
 
 // baseUrl: "http://172.16.227.175:3000"
+// baseUrl: "http://192.168.4.162:3000",
 
 export default {
-    baseUrl: "http://192.168.4.162:3000",
+    baseUrl: 'http://54.215.197.214:80/'
 }
