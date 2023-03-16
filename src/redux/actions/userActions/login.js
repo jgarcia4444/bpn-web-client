6e0e86f091adfe1828025e7865012550c245ff5d
Jake Garcia <bballwiz4@gmail.com>
2023-03-15 17:26:01 -0700

    The new domain name worked on the test site(localhost).

--- src/redux/actions/userActions/login.js
+++ src/redux/actions/userActions/login.js
@@ -3,13 +3,13 @@ import Urls from "../../../config/network/Urls";
 const {baseUrl} = Urls;
 
 const login = userInfo => {
+    console.log("login triggered")
     let url = `${baseUrl}/users/login`;
     let options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        mode: 'no-cors',
         body: JSON.stringify({login_info: userInfo})
     };
     return async dispatch => {
@@ -17,6 +17,7 @@ const login = userInfo => {
         fetch(url, options)
             .then(res => res.json())
             .then(data => {
+                console.log("response from login", data);
                 let {success} = data;
                 if (success === true) {
                     let {userInfo} = data;
